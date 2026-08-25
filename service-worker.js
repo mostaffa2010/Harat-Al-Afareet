a3f64ae2b3be51adf0a05fa3467895cf4030b4c6
mostaffa2010 <71957057+mostaffa2010@users.noreply.github.com>
2026-08-25 08:08:32 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,115 @@
+/**
+ * حارة العفاريت — Harat El Afareet
+ * PWA Service Worker for Offline Caching
+ */
+
+const CACHE_NAME = 'harat-el-afareet-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './styles.css',
+    './manifest.json',
+    './assets/icon-192.png',
+    './assets/icon-512.png',
+    './js/core/game.js',
+    './js/core/gameLoop.js',
+    './js/core/renderer.js',
+    './js/core/assetManager.js',
+    './js/data/constants.js',
+    './js/data/defaultData.js',
+    './js/systems/inputSystem.js',
+    './js/systems/cameraSystem.js',
+    './js/systems/collisionSystem.js',
+    './js/systems/spawnSystem.js',
+    './js/systems/waveSystem.js',
+    './js/systems/difficultySystem.js',
+    './js/systems/xpSystem.js',
+    './js/systems/upgradeSystem.js',
+    './js/systems/damageSystem.js',
+    './js/systems/particleSystem.js',
+    './js/systems/audioSystem.js',
+    './js/systems/saveSystem.js',
+    './js/entities/player.js',
+    './js/entities/projectile.js',
+    './js/entities/pickup.js',
+    './js/entities/baseEnemy.js',
+    './js/characters/characterRegistry.js',
+    './js/characters/apprentice.js',
+    './js/characters/fireMage.js',
+    './js/characters/amuletKeeper.js',
+    './js/weapons/baseWeapon.js',
+    './js/weapons/weaponRegistry.js',
+    './js/weapons/magicStaff.js',
+    './js/weapons/fireWand.js',
+    './js/weapons/lightningRod.js',
+    './js/weapons/magicalTalisman.js',
+    './js/enemies/enemyRegistry.js',
+    './js/enemies/smallAfreet.js',
+    './js/enemies/fastAfreet.js',
+    './js/enemies/rangedAfreet.js',
+    './js/enemies/giantAfreet.js',
+    './js/bosses/bossRegistry.js',
+    './js/bosses/afreetKing.js',
+    './js/upgrades/upgradeRegistry.js',
+    './js/upgrades/weapons/fireDamage.js',
+    './js/upgrades/weapons/projectileCount.js',
+    './js/upgrades/weapons/lightningFork.js',
+    './js/upgrades/weapons/talismanOrbitSpeed.js',
+    './js/upgrades/weapons/staffHoming.js',
+    './js/upgrades/player/movementSpeed.js',
+    './js/upgrades/player/maxHealth.js',
+    './js/upgrades/player/pickupRange.js',
+    './js/upgrades/player/armorShield.js',
+    './js/upgrades/player/healthRegen.js',
+    './js/upgrades/general/criticalChance.js',
+    './js/upgrades/general/attackSpeed.js',
+    './js/upgrades/general/projectileSpeed.js',
+    './js/upgrades/general/areaOfEffect.js',
+    './js/upgrades/general/xpBoost.js',
+    './js/upgrades/general/goldDigger.js',
+    './js/ui/uiManager.js',
+    './js/ui/mainMenu.js',
+    './js/ui/characterSelect.js',
+    './js/ui/hud.js',
+    './js/ui/levelUpModal.js',
+    './js/ui/pauseMenu.js',
+    './js/ui/gameOverModal.js',
+    './js/ui/victoryModal.js',
+    './js/ui/bazaarModal.js',
+    './js/ui/collectionModal.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache and caching all game assets');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Clearing old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Return cached asset if available, else fetch from network
+            return response || fetch(event.request);
+        })
+    );
+});
